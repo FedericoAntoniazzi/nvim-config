6ef76332d6e90be42e98746104fba1c3df0925ad
Federico Antoniazzi <federico.antoniazzi.000@gmail.com>
2021-12-09 10:10:33 +0100

    [FIX] nvim cmp snippets, completions and plugins

--- lua/completion.lua
+++ lua/completion.lua
@@ -19,6 +19,8 @@ cmp.setup({
     ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
     ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
     ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+    ["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item()),
+    ["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item()),
     ['<C-e>'] = cmp.mapping({
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
@@ -51,6 +53,8 @@ cmp.setup({
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
+  }, {
+    { name = 'treesitter' }
   }, {
     { name = 'buffer' },
   })
