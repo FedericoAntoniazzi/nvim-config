de9958652590f4f382f8326f9c0c8371ae76adf2
Federico Antoniazzi <federico.antoniazzi.000@gmail.com>
2022-06-22 23:29:57 +0200

    feat(yamlls): ansible schemas

--- lua/lsp/yaml.lua
+++ lua/lsp/yaml.lua
@@ -1,16 +1,22 @@
 local yaml = {}
 
 function yaml.setup(on_attach, capabilities)
-  require'lspconfig'.yamlls.setup{
-    on_attach=on_attach,
+  require 'lspconfig'.yamlls.setup {
+    on_attach = on_attach,
     capabilities = capabilities,
-    filetypes = {"yaml", "yaml.ansible", "yaml.docker-compose"},
+    filetypes = { "yaml", "yaml.ansible", "yaml.docker-compose" },
     settings = {
       yaml = {
         schemas = {
           ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
           ["https://json.schemastore.org/chart.json"] = "*/charts/*",
-          kubernetes = "*/manifests/*"
+          ["https://json.schemastore.org/kustomization.json"] = "kustomization(s|.yaml)",
+          kubernetes = "*/manifests/*",
+
+          -- Ansible
+          ["https://raw.githubusercontent.com/ansible-community/schemas/main/f/ansible.json#/$defs/tasks"] = "*/tasks/*",
+          ["https://raw.githubusercontent.com/ansible-community/schemas/main/f/ansible-vars.json"] = "/(defaults|vars)/*",
+          ["https://raw.githubusercontent.com/ansible-community/schemas/main/f/ansible-requirements.json"] = "roles/**/requirements.txt"
         }
       }
     }
