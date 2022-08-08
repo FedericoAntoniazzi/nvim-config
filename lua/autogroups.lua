8e3cb5cac1b163158284468f6a64798b0be34dc4
Federico Antoniazzi <federico.antoniazzi.000@gmail.com>
2022-08-08 18:15:09 +0200

    feat: add run, test and build mappings for Go

--- lua/autogroups.lua
+++ lua/autogroups.lua
@@ -3,3 +3,9 @@ local utils = require('utils')
 utils.create_augroup('Lua', {
   { 'Filetype', 'lua', 'set tabstop=2 expandtab shiftwidth=2 softtabstop=2' }
 })
+
+utils.create_augroup('Golang', {
+  { 'Filetype go', 'nmap', '<leader>rr', '<cmd>:GoRun<CR>' },
+  { 'Filetype go', 'nmap', '<leader>t', '<cmd>:GoTest<CR>' },
+  { 'Filetype go', 'nmap', '<leader>b', '<cmd>:GoBuild<CR>' }
+})
