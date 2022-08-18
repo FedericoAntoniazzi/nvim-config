6c8c863758dc0cf4b31aeaaf0a516746235e6707
Federico Antoniazzi <federico.antoniazzi.000@gmail.com>
2022-08-18 15:39:48 +0200

    feat(lsp): add ansiblels and yaml schemas

--- lua/config/lsps/yaml.lua
+++ lua/config/lsps/yaml.lua
@@ -10,8 +10,21 @@ function Yaml.setup(on_attach, capabilities, flags)
     settings = {
       yaml = {
         schemas = {
+          -- GitLab
           ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = ".gitlab-ci.{yaml,yml}",
-          ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*"
+
+          -- GitHub
+          ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+
+          -- Ansible
+          ["https://raw.githubusercontent.com/ansible-community/schemas/main/f/ansible.json#/$defs/tasks"] = "*/tasks/*",
+          ["https://raw.githubusercontent.com/ansible-community/schemas/main/f/ansible-vars.json"] = "/(defaults|vars)/*",
+          ["https://raw.githubusercontent.com/ansible-community/schemas/main/f/ansible-meta.json"] = "/meta/meta.y{a,}ml",
+          ["https://raw.githubusercontent.com/ansible-community/schemas/main/f/ansible-meta-runtime.json"] = "/meta/runtime.y{a,}ml",
+          ["https://raw.githubusercontent.com/ansible-community/schemas/main/f/ansible-inventory.json"] = "/inventory/**/*.y{a,}ml",
+
+          -- Docker compose
+          ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "docker-compose.y{a,}ml"
         }
       }
     }
