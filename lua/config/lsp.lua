61a56d33df9ebf16f29d4187c33ca894c7947787
Federico Antoniazzi <federico.antoniazzi.000@gmail.com>
2023-04-14 11:20:33 +0200

    fix: deprecate formatting function

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -13,7 +13,7 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<leader>f', vim.lsp.buf.formatting, bufopts)
+  vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, bufopts)
 
   -- Signature
   require('lsp_signature').on_attach({
