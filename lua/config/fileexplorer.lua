f8d60251f6a287a38135b30c8883385e87468632
Federico Antoniazzi <federico.antoniazzi.000@gmail.com>
2023-04-14 10:43:43 +0200

    feat: update nvim-tree

--- lua/config/fileexplorer.lua
+++ lua/config/fileexplorer.lua
@@ -1,8 +1,9 @@
 require('nvim-tree').setup({
-  open_on_setup = true,
   sync_root_with_cwd = true,
   hijack_cursor = true,
   view = {
     adaptive_size = true,
   }
 })
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = require("nvim-tree.api").tree.open })
