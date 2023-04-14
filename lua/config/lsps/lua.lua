38b4fa0887757b7e15e739104371caf072cfbd38
Federico Antoniazzi <federico.antoniazzi.000@gmail.com>
2023-04-14 10:42:51 +0200

    feat: update lua lsp

--- lua/config/lsps/lua.lua
+++ lua/config/lsps/lua.lua
@@ -1,18 +1,30 @@
 local lua = {}
 
 function lua.setup(on_attach, capabilities, flags)
-  require('lspconfig').sumneko_lua.setup {
+  require'lspconfig'.lua_ls.setup {
     on_attach = on_attach,
     capabilities = capabilities,
     flags = flags,
-
     settings = {
       Lua = {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+          version = 'LuaJIT',
+        },
         diagnostics = {
-          globals = { 'vim' },
-        }
-      }
-    }
+          -- Get the language server to recognize the `vim` global
+          globals = {'vim'},
+        },
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = vim.api.nvim_get_runtime_file("", true),
+        },
+        -- Do not send telemetry data containing a randomized but unique identifier
+        telemetry = {
+          enable = false,
+        },
+      },
+    },
   }
 end
 
