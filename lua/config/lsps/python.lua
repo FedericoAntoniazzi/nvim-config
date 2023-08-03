5012f27107bd3f8e280ddd867d6e381da6ecce7a
Federico Antoniazzi <federico.antoniazzi.000@gmail.com>
2023-08-03 10:31:25 +0200

    fix: python lsp

--- lua/config/lsps/python.lua
+++ lua/config/lsps/python.lua
@@ -10,18 +10,22 @@ function python.setup(on_attach, capabilities, flags)
       pylsp = {
         configurationSources = {"flake8"},
         plugins = {
-          pycodestyle = {enabled = false},
-          mccabe = {enabled = false},
-          pyflakes = {enabled = false},
-          flake8 = {
-            enabled = true,
-            config = ".flake8",
-            maxLineLength = 120
-          },
           black = {
             enabled = true,
             line_length = 120
-          }
+          },
+          mccabe = {
+            enabled = true
+          },
+          flake8 = {
+            enabled = true,
+            config = ".flake8",
+            maxLineLength = 120,
+            indentSize = 4
+          },
+          pycodestyle = {
+            enabled = false
+          },
         }
       }
     }
