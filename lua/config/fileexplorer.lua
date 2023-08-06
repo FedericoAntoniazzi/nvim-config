0e9dc67e3f7a36d02f2b2670617a4cf5be73c0d0
Federico Antoniazzi <federico.antoniazzi.000@gmail.com>
2023-08-06 14:41:43 +0200

    fix: auto close nvim-tree if last window

--- lua/config/fileexplorer.lua
+++ lua/config/fileexplorer.lua
@@ -30,3 +30,12 @@ end
 
 -- Open nvim-tree at startup
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  nested = true,
+  callback = function()
+    if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
+      vim.cmd "quit"
+    end
+  end
+})
