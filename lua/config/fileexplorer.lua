0ad5646530cac0fa22cf381bee84a0b9f3fc67e0
Federico Antoniazzi <federico.antoniazzi.000@gmail.com>
2023-08-10 14:54:40 +0200

    fix: auto close nvim-tree if last window

--- lua/config/fileexplorer.lua
+++ lua/config/fileexplorer.lua
@@ -17,25 +17,25 @@ local function open_nvim_tree(data)
   -- buffer is a directory
   local directory = vim.fn.isdirectory(data.file) == 1
 
+  if directory then
+    vim.cmd.cd(data.file)
+    require("nvim-tree.api").tree.open()
+  end
+
   -- buffer is a [No Name]
   local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
 
-  if not directory and not no_name then
-    return
+  if no_name then
+    -- open the tree, find the file but don't focus it
+    require("nvim-tree.api").tree.toggle({ focus = false, find_file = true, })
   end
-
-  -- open the tree, find the file but don't focus it
-  require("nvim-tree.api").tree.toggle({ focus = false, find_file = true, })
 end
 
 -- Open nvim-tree at startup
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
-vim.api.nvim_create_autocmd("BufEnter", {
-  nested = true,
-  callback = function()
-    if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
-      vim.cmd "quit"
-    end
+vim.api.nvim_create_autocmd("QuitPre", {
+  callback = function ()
+    vim.cmd("NvimTreeClose")
   end
 })
