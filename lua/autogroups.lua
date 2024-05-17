44f39b1032b01769440bcec5e82688b464d019f1
FedericoAntoniazzi <federico.antoniazzi@sighup.io>
2024-05-17 14:36:58 +0200

    chore: remove unused nomad config

--- lua/autogroups.lua
+++ lua/autogroups.lua
@@ -10,12 +10,10 @@ utils.create_augroup('Golang', {
   { 'Filetype go', 'nmap', '<leader>b', '<cmd>:GoBuild<CR>' }
 })
 
-utils.create_augroup('HCL', {
-  -- Manually set hcl filetype on .nomad files
-  { 'BufEnter,BufNew', '*.nomad', 'set filetype=hcl' },
-  { 'Filetype', 'hcl', 'set tabstop=2 expandtab shiftwidth=2 softtabstop=2' }
-})
-
 utils.create_augroup('Terraform', {
   { 'Filetype', 'terraform', 'set tabstop=2 expandtab shiftwidth=2 softtabstop=2' }
+})
+
+utils.create_augroup('yaml', {
+  { 'Filetype yaml', 'nmap', '<leader>y', '<cmd>:Telescope yaml_schema<CR>' }
 })
