3e9f20fa94e18fd0d1f50caf5d1993ab58d91855
FedericoAntoniazzi <federico.antoniazzi@sighup.io>
2024-05-17 16:06:49 +0200

    feat: autogroup for lua

--- init.lua
+++ init.lua
@@ -1,1 +1,11 @@
 require('settings')
+
+local augrp = vim.api.nvim_create_augroup
+local aucmd = vim.api.nvim_create_autocmd
+
+local lua = augrp('lua', { clear = true })
+aucmd({'FileType'}, {
+  pattern = "lua",
+  group = lua,
+  command = 'set tabstop=2 expandtab shiftwidth=2 softtabstop=2',
+})
