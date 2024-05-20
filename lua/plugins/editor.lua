ad70d74d9fccdd9f84135d86b818855727372028
FedericoAntoniazzi <federico.antoniazzi@sighup.io>
2024-05-20 11:39:39 +0200

    feat: install telescope

---
+++ lua/plugins/editor.lua
@@ -0,0 +1,18 @@
+return {
+  -- Quick text search
+  {
+    'nvim-telescope/telescope.nvim',
+    version = "v0.1.6",
+    event = {"VeryLazy"},
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    keys = {
+      {"<leader>ff", ":lua require('telescope.builtin').find_files()<CR>", desc="Find files", mode="n", silent=true, noremap=true},
+      {"<leader>fg", ":lua require('telescope.builtin').live_grep()<CR>", desc="Grep files", mode="n", silent=true, noremap=true},
+      {"<leader>fb", ":lua require('telescope.builtin').buffers()<CR>", desc="Show buffers", mode="n", silent=true, noremap=true},
+      {"<leader>ft", ":lua require('telescope.builtin').treesitter()<CR>", desc="List function names and variables", mode="n", silent=true, noremap=true},
+      {"<leader>fc", ":lua require('telescope.builtin').git_commits()<CR>", desc="Lists git commits with diff preview", mode="n", silent=true, noremap=true},
+    },
+  },
+}
