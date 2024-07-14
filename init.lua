a8e86f9291699093e738f7c633053f6ca4abf14c
Federico Antoniazzi <federico.antoniazzi.000@gmail.com>
2024-07-14 23:16:20 +0200

    chore: initial config

---
+++ init.lua
@@ -0,0 +1,8 @@
+-- Core settings
+require("core")
+
+-- Plugins
+require("config.lazy")
+
+-- Colorscheme
+vim.cmd.colorscheme("catppuccin")
---
+++ lua/core/system.lua
@@ -0,0 +1,6 @@
+vim.opt.shell = "/bin/zsh"
+vim.opt.clipboard = "unnamedplus"
+
+-- Disable swapfile and backup
+vim.opt.swapfile = false
+vim.opt.backup = false
