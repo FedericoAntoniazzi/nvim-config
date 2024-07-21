eb0854ab2abaaeadc3c8066cfe261631caa9cb89
Federico Antoniazzi <federico.antoniazzi.000@gmail.com>
2024-07-21 23:56:05 +0200

    feat: highlight code with treesitter

---
+++ lua/plugins/code/treesitter.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+        ensure_installed = { 'bash', 'dockerfile', 'go', 'gomod', 'json', 'hcl', 'python', 'yaml' },
+      })
+    end
+  }
+}
