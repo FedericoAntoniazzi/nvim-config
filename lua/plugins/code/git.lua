23468a34fe53efebffd5381bc56939aca93a6760
Federico Antoniazzi <federico.antoniazzi.000@gmail.com>
2024-07-21 23:44:10 +0200

    feat: show git signs

---
+++ lua/plugins/code/git.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '-' },
+      },
+      current_line_blame = true,
+    }
+  }
+}
