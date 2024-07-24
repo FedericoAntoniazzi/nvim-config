37f75eeb939f3bb1ef7ccf84ef198c75b50d72c9
Federico Antoniazzi <federico.antoniazzi.000@gmail.com>
2024-07-24 22:51:27 +0200

    feat: mason.nvim package manager

--- lua/plugins/code/lsp.lua
+++ lua/plugins/code/lsp.lua
@@ -1,5 +1,37 @@
 return {
   {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        build = ":MasonUpdate",
+        opts = {}
+      }
+    },
+    config = function()
+      local joinLists = function(t1, t2)
+        for k, v in pairs(t2) do
+          t1[k] = v
+        end
+        return t1
+      end
+
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          'ansiblels',
+          'bashls',
+          'dockerls',
+          'golangci_lint_ls',
+          'gopls',
+          'jsonls',
+          'pylsp',
+          'terraformls',
+          'yamlls'
+        }
+      })
+    end
+  },
+  {
     "neovim/nvim-lspconfig",
     event = { "BufReadPre", "BufNewFile" },
     dependencies = {
@@ -10,23 +42,32 @@ return {
       -- local protocol = require("vim.lsp.protocol")
 
       local on_attach = function(client, bufnr)
-          -- format on save
-          if client.server_capabilities.documentFormattingProvider then
-              vim.api.nvim_create_autocmd("BufWritePre", {
-                  group = vim.api.nvim_create_augroup("Format", { clear = true }),
-                  buffer = bufnr,
-                  callback = function()
-                      vim.lsp.buf.format()
-                  end,
-              })
-          end
+        -- format on save
+        if client.server_capabilities.documentFormattingProvider then
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = vim.api.nvim_create_augroup("Format", { clear = true }),
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format()
+            end,
+          })
+        end
       end
 
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
       -- Use a loop to conveniently call 'setup' on multiple servers and
       -- map buffer local keybindings when the language server attaches
-      local servers = { 'ansiblels', 'bashls', 'dockerls', 'gopls', 'golangci_lint_ls', 'terraformls' }
+      local servers = {
+        'ansiblels',
+        'bashls',
+        'dockerls',
+        'golangci_lint_ls',
+        'gopls',
+        'jsonls',
+        'terraformls',
+
+      }
       for _, lsp in ipairs(servers) do
         lspconfig[lsp].setup({
           on_attach = on_attach,
