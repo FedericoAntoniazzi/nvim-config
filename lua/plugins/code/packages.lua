cf0b5a23cd284de9ba0f734df340f30cb323d029
Federico Antoniazzi <federico.antoniazzi.000@gmail.com>
2024-07-26 18:13:52 +0200

    chore: split mason in single file

---
+++ lua/plugins/code/packages.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        build = ":MasonUpdate",
+        opts = {}
+      },
+      {
+        "WhoIsSethDaniel/mason-tool-installer.nvim"
+      }
+    },
+
+    config = function()
+      local lsps = {
+        'ansiblels',
+        'bashls',
+        'dockerls',
+        'golangci_lint_ls',
+        'gopls',
+        'jsonls',
+        'pylsp',
+        'terraformls',
+        'yamlls'
+      }
+
+      require("mason-tool-installer").setup({
+        ensure_installed = lsps
+      })
+    end
+  },
+}
