b95495280796f00b237f654b4f4c04a91a8fd342
Federico Antoniazzi <federico.antoniazzi.000@gmail.com>
2024-07-26 18:29:15 +0200

    chore: VeryLazy yaml companion

--- lua/plugins/code/lsp.lua
+++ lua/plugins/code/lsp.lua
@@ -50,6 +50,7 @@ return {
   },
   {
     "someone-stole-my-name/yaml-companion.nvim",
+    event = { "VeryLazy" },
     config = function()
       require("telescope").load_extension("yaml_schema")
     end
