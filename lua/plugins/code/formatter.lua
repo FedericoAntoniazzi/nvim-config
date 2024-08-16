281e20973bd1113d07950722e0ac35ebfcfc6c05
Federico Antoniazzi <federico.antoniazzi.000@gmail.com>
2024-08-16 12:33:31 +0200

    chore: replace format on save with explicit command

--- lua/plugins/code/formatter.lua
+++ lua/plugins/code/formatter.lua
@@ -7,11 +7,23 @@ return {
         formatters_by_ft = {
           yaml = { "yamlfmt", "yamlfix" }
         },
-        format_on_save = {
-          timeout_ms = 500,
-          lsp_fallback = true,
-        }
+        -- format_on_save = {
+        --   timeout_ms = 500,
+        --   lsp_fallback = true,
+        -- }
       })
+
+      vim.api.nvim_create_user_command("Format", function(args)
+        local range = nil
+        if args.count ~= -1 then
+          local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+          range = {
+            start = { args.line1, 0 },
+            ["end"] = { args.line2, end_line:len() },
+          }
+        end
+        require("conform").format({ async = true, lsp_format = "fallback", range = range })
+      end, { range = true })
     end
   }
 }
