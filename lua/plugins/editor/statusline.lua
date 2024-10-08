2fd414c3a59a6a631730a3473f09322f6af8ea03
Federico Antoniazzi <federico.antoniazzi.000@gmail.com>
2024-10-08 23:27:13 +0200

    chore: vertical status line separators

--- lua/plugins/editor/statusline.lua
+++ lua/plugins/editor/statusline.lua
@@ -3,8 +3,20 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {
+      options = {
+        component_separators = { left = "|", right = "|" },
+        section_separators = { left = "", right = "" }
+      },
       extensions = {
         'nvim-tree',
+      },
+      tabline = {
+        lualine_a = {
+          {
+            'buffers',
+            mode = 2
+          }
+        }
       }
     }
   }
